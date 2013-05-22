271a055e436b15ca09b76fb48096e86c1881261c
Bogdan Gradinariu <bogdan.gradinariu@xivic.com>
2013-05-22 18:49:37 +0300

    angular bootstrap

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('soundmapApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
